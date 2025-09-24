72123144045ce08eddd329024b231e3fdea0755b
James Randall <james@manmachineltd.com>
2025-09-24 22:00:23 +1000

    added name to json_data table

--- database/migrations/2025_09_23_030503_create_json_data_table.php
+++ database/migrations/2025_09_23_030503_create_json_data_table.php
@@ -13,6 +13,7 @@ return new class() extends Migration {
         Schema::create('json_data', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name')->unique();
             $table->string('filename')->unique();
             $table->string('original_filename');
             $table->integer('file_size');
