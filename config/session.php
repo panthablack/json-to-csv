41b00551b962bd23902967283cddb6f3d0f677ff
James Randall <james@manmachineltd.com>
2025-09-25 08:36:01 +1000

    Removed default laravel references

--- config/session.php
+++ config/session.php
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'json_to_csv'), '_') . '_session'
     ),
 
     /*
